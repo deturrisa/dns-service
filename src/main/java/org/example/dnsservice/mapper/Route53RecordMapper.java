57e02ebc390836b46f872e0773a98d62b1250fca
alexdeturris <alexdeturris@hotmail.com>
2024-12-22 21:25:11 +0000

    improve mapper

--- src/main/java/org/example/dnsservice/mapper/Route53RecordMapper.java
+++ src/main/java/org/example/dnsservice/mapper/Route53RecordMapper.java
@@ -6,6 +6,7 @@ import org.example.dnsservice.model.SupportedCountryCode;
 import org.example.dnsservice.service.AwsR53Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.route53.model.ListResourceRecordSetsResponse;
 import software.amazon.awssdk.services.route53.model.RRType;
 import software.amazon.awssdk.services.route53.model.ResourceRecord;
 import software.amazon.awssdk.services.route53.model.ResourceRecordSet;
@@ -20,9 +21,6 @@ public class Route53RecordMapper {
 
     private final R53Properties r53Properties;
 
-    //TODO resolve this programatically
-    private static final String DOMAIN_COM = "domain.com.";
-
     @Autowired
     public Route53RecordMapper(AwsR53Service awsR53Service, R53Properties r53Properties) {
         this.awsR53Service = awsR53Service;
@@ -30,31 +28,57 @@ public class Route53RecordMapper {
     }
 
     public List<ARecord> getRoute53Records() {
-        return getANameRecords().stream().flatMap(
+        ListResourceRecordSetsResponse response = getListResourceRecordSetsResponse();
+        return getANameRecords(response).stream().flatMap(
             resourceRecordSet -> resourceRecordSet.resourceRecords().stream().map(
-                    resourceRecord -> createARecord(resourceRecordSet, resourceRecord)
+                    resourceRecord -> createARecord(
+                            resourceRecordSet,
+                            resourceRecord,
+                            response
+                    )
             )
         ).toList();
     }
 
-    private static ARecord createARecord(ResourceRecordSet resourceRecordSet, ResourceRecord resourceRecord) {
+    private static ARecord createARecord(
+            ResourceRecordSet resourceRecordSet,
+            ResourceRecord resourceRecord,
+            ListResourceRecordSetsResponse response
+    ) {
         return new ARecord(
                 resourceRecordSet.name(),
-                getCountryDomain(resourceRecordSet),
+                getCountryDomain(resourceRecordSet,response),
                 resourceRecord.value()
         );
     }
 
-    private static String getCountryDomain(ResourceRecordSet resourceRecordSet) {
+    private static String getCountryDomain(
+            ResourceRecordSet resourceRecordSet,
+            ListResourceRecordSetsResponse response
+    ) {
+        String hostedZoneName = response.resourceRecordSets().stream().filter(
+                recordSet -> recordSet.type().equals(RRType.SOA)
+        ).map(ResourceRecordSet::name).findFirst().orElse(null);
+         //TODO handle exception
+        assert hostedZoneName != null;
         return SupportedCountryCode
                 .valueOf(
                         resourceRecordSet.geoLocation().countryCode()
-                ).getSubdomain().concat(".").concat(DOMAIN_COM);
+                ).getSubdomain().concat(".").concat(hostedZoneName);
     }
 
 
-    private List<ResourceRecordSet> getANameRecords(){
-        return awsR53Service.getResourceRecordSets(r53Properties.hostedZoneId()).join().resourceRecordSets().stream().filter(recordSet -> recordSet.type().equals(RRType.A))
+    private List<ResourceRecordSet> getANameRecords(ListResourceRecordSetsResponse response){
+        return response
+                .resourceRecordSets().stream().filter(
+                        recordSet -> recordSet.type().equals(RRType.A)
+                )
                 .collect(Collectors.toList());
+    }
+
+    private ListResourceRecordSetsResponse getListResourceRecordSetsResponse() {
+        return awsR53Service.getResourceRecordSets(
+                r53Properties.hostedZoneId()
+        ).join();
     }
 }
