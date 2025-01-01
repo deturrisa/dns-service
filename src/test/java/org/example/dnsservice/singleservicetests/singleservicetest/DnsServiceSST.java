0dad28a02258837182499396d227e28ce4752b0d
alexdeturris <alexdeturris@hotmail.com>
2025-01-01 14:56:17 +0000

    Refactor DNS service test setup and assertions.
    Simplified test data setup by restructuring cluster and server entities. Enhanced assertions to check updated server table state with more comprehensive conditions.

--- src/test/java/org/example/dnsservice/singleservicetests/singleservicetest/DnsServiceSST.java
+++ src/test/java/org/example/dnsservice/singleservicetests/singleservicetest/DnsServiceSST.java
@@ -8,17 +8,13 @@ import org.example.dnsservice.model.Action;
 import org.example.dnsservice.singleservicetests.BaseSST;
 import org.example.dnsservice.singleservicetests.ExternalPlatform;
 import org.example.dnsservice.singleservicetests.SingleServiceTest;
-import org.example.dnsservice.util.TestUtil.ResourceRecordSetTestData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import software.amazon.awssdk.services.route53.model.ListResourceRecordSetsResponse;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
-
 import static org.example.dnsservice.util.TestUtil.ResourceRecordSetTestData.*;
 import static org.example.dnsservice.util.TestUtil.ResourceRecordSetTestData.FRANKFURT;
 import static org.mockito.Mockito.when;
@@ -32,6 +28,8 @@ public class DnsServiceSST extends BaseSST {
 
     @BeforeEach
     public void setup() {
+        serverRepository.deleteAll();
+        clusterRepository.deleteAll();
         when(domainRegionProperties.getDomainRegions()).thenReturn(
                 List.of(
                         new DomainRegion(USA, Set.of(LA, NYC)),
@@ -45,31 +43,54 @@ public class DnsServiceSST extends BaseSST {
     @Test
     public void testShouldRenderServerTableWithTwoRecordsToAddToRotation() throws Exception {
         //given
-        ClusterEntity hkClusterEntity = new ClusterEntity(4,"Hong Kong","hongkong");
-        ServerEntity hkServer1 = new ServerEntity(4,"rackspace-1","234.234.234.234",hkClusterEntity);
-        ServerEntity hkServer2 = new ServerEntity(5,"rackspace-2","235.235.235.235",hkClusterEntity);
+        ClusterEntity laClusterEntity = new ClusterEntity(1,"Los Angeles",LA);
+        ServerEntity laServer1 = new ServerEntity(1,"ubiq-1","123.123.123.123", laClusterEntity);
+        ServerEntity laServer2 = new ServerEntity(2,"ubiq-2","125.125.125.125", laClusterEntity);
 
-        clusterRepository.save(hkClusterEntity);
-        serverRepository.saveAll(List.of(hkServer1,hkServer2));
+        ClusterEntity genevaClusterEntity = new ClusterEntity(5,"Geneva", GENEVA);
+        ServerEntity genevaServer = new ServerEntity(20,"some-friendly-name-1","9.9.9.9", genevaClusterEntity);
+
+        clusterRepository.saveAll(List.of(laClusterEntity, genevaClusterEntity));
+        serverRepository.saveAll(List.of(laServer1, laServer2, genevaServer));
+
+        ListResourceRecordSetsResponse response =
+                createListResourceRecordSetsResponse(
+                        List.of(
+                                getNsResourceRecordSet(),
+                                getSoaResourceRecordSet(),
+                                getUsaAResourceRecordSet(LA,
+                                        List.of("123.123.123.123", "125.125.125.125")
+                                ),
+                                getSwitzerlandAResourceRecordSet(GENEVA,
+                                        List.of("9.9.9,9")
+                                )
+                        )
+                );
 
         when(awsR53Service.getResourceRecordSets(r53Properties.hostedZoneId())).thenReturn(
-                CompletableFuture.completedFuture(ResourceRecordSetTestData.getDefaultResourceRecordSetsResponse())
+                CompletableFuture.completedFuture(response)
         );
 
         //when
         //then
+        int expectedRowCountWithHeader = 4;
         getDnsServiceHomePage()
-                .andExpect(xpath("//tr").nodeCount(3))
-                .andExpect(xpath("//tr[1]/td[1]").string("server" + hkServer1.getId()))
-                .andExpect(xpath("//tr[1]/td[2]").string(hkClusterEntity.getSubdomain()))
-                .andExpect(xpath("//tr[1]/td[3]").string("NONE"))
-                .andExpect(xpath("//tr[1]/td[4]").string(Action.ADD.getDescription()))
-                .andExpect(xpath("//tr[2]/td[1]").string("server" + hkServer2.getId()))
-                .andExpect(xpath("//tr[2]/td[2]").string(hkClusterEntity.getSubdomain()))
-                .andExpect(xpath("//tr[2]/td[3]").string("NONE"))
-                .andExpect(xpath("//tr[2]/td[4]").string(Action.ADD.getDescription()));
-
-
+                //server1
+                .andExpect(xpath("//tr").nodeCount(expectedRowCountWithHeader))
+                .andExpect(xpath("//tr[1]/td[1]").string("server" + laServer1.getId()))
+                .andExpect(xpath("//tr[1]/td[2]").string(USA))
+                .andExpect(xpath("//tr[1]/td[3]").string(USA + DOT_DOMAIN_COM))
+                .andExpect(xpath("//tr[1]/td[4]").string(Action.REMOVE.getDescription()))
+                //server2
+                .andExpect(xpath("//tr[2]/td[1]").string("server" + laServer2.getId()))
+                .andExpect(xpath("//tr[2]/td[2]").string(USA))
+                .andExpect(xpath("//tr[2]/td[3]").string(USA + DOT_DOMAIN_COM))
+                .andExpect(xpath("//tr[2]/td[4]").string(Action.REMOVE.getDescription()))
+                //server20
+                .andExpect(xpath("//tr[3]/td[1]").string("server" + genevaServer.getId()))
+                .andExpect(xpath("//tr[3]/td[2]").string(SWITZERLAND))
+                .andExpect(xpath("//tr[3]/td[3]").string("NONE"))
+                .andExpect(xpath("//tr[3]/td[4]").string(Action.ADD.getDescription()));
     }
 
 }
