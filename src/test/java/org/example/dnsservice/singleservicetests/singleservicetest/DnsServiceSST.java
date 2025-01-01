f14676ffda4e60713634cb4b79cd5c9aec28368b
alexdeturris <alexdeturris@hotmail.com>
2025-01-01 19:51:28 +0000

    Refactor DNS and server table rendering logic.
    Separated DNS and server entries into distinct tables for clarity. Updated test cases to validate the individual tables and improved reusability by introducing helper methods for table assertions. Minor enhancements made to data representation in the UI templates.

--- src/test/java/org/example/dnsservice/singleservicetests/singleservicetest/DnsServiceSST.java
+++ src/test/java/org/example/dnsservice/singleservicetests/singleservicetest/DnsServiceSST.java
@@ -11,6 +11,7 @@ import org.example.dnsservice.singleservicetests.SingleServiceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.ResultActions;
 import software.amazon.awssdk.services.route53.model.ListResourceRecordSetsResponse;
 import java.util.List;
 import java.util.Set;
@@ -35,62 +36,111 @@ public class DnsServiceSST extends BaseSST {
                         new DomainRegion(USA, Set.of(LA, NYC)),
                         new DomainRegion(SWITZERLAND, Set.of(GENEVA)),
                         new DomainRegion(HONG_KONG, Set.of(HONG_KONG)),
-                        new DomainRegion(GERMANY, Set.of(FRANKFURT))
+                        new DomainRegion(GERMANY, Set.of(FRANKFURT)),
+                        new DomainRegion("abc", Set.of("xyz"))
                 )
         );
     }
 
+    private static final ClusterEntity laClusterEntity = new ClusterEntity(1,"Los Angeles",LA);
+    private static final String laIp1 = "123.123.123.123";
+    private static final ServerEntity laServer1 = new ServerEntity(1,"ubiq-1", laIp1, laClusterEntity);
+    private static final String laIp2 = "125.125.125.125";
+    private static final ServerEntity laServer2 = new ServerEntity(2,"ubiq-2", laIp2, laClusterEntity);
+
+    private static final ClusterEntity genevaClusterEntity = new ClusterEntity(5,"Geneva", GENEVA);
+    private static final String genevaIp = "9.9.9.9";
+    private static final ServerEntity genevaServer = new ServerEntity(20,"some-friendly-name-1", genevaIp, genevaClusterEntity);
+
     @Test
     public void testShouldRenderServerEntries() throws Exception {
         //given
-        ClusterEntity laClusterEntity = new ClusterEntity(1,"Los Angeles",LA);
-        ServerEntity laServer1 = new ServerEntity(1,"ubiq-1","123.123.123.123", laClusterEntity);
-        ServerEntity laServer2 = new ServerEntity(2,"ubiq-2","125.125.125.125", laClusterEntity);
-
-        ClusterEntity genevaClusterEntity = new ClusterEntity(5,"Geneva", GENEVA);
-        ServerEntity genevaServer = new ServerEntity(20,"some-friendly-name-1","9.9.9.9", genevaClusterEntity);
-
         clusterRepository.saveAll(List.of(laClusterEntity, genevaClusterEntity));
         serverRepository.saveAll(List.of(laServer1, laServer2, genevaServer));
 
-        ListResourceRecordSetsResponse response =
+        ListResourceRecordSetsResponse listResourceRecordSetsResponse =
                 createListResourceRecordSetsResponse(
                         List.of(
                                 getNsResourceRecordSet(),
                                 getSoaResourceRecordSet(),
-                                getUsaAResourceRecordSet(LA,
-                                        List.of("123.123.123.123", "125.125.125.125")
+                                getUsaAResourceRecordSet(
+                                        LA,
+                                        List.of(laIp1, laIp2)
                                 ),
-                                getSwitzerlandAResourceRecordSet(GENEVA,
-                                        List.of("9.9.9,9")
+                                createAResourceRecordSet(
+                                        "abc" + DOT_DOMAIN_COM,
+                                        "xyz",
+                                        createIpResourceRecords(List.of("5.5.5.5"))
                                 )
                         )
                 );
 
         when(awsR53Service.getResourceRecordSets(r53Properties.hostedZoneId())).thenReturn(
-                CompletableFuture.completedFuture(response)
+                CompletableFuture.completedFuture(listResourceRecordSetsResponse)
         );
 
         //when
+        ResultActions response = getDnsServiceHomePage();
+
         //then
-        int expectedRowCountWithHeader = 4;
-        getDnsServiceHomePage()
+        assertServerEntryTable(response);
+        assertDnsEntryTable(response);
+    }
+
+    private static void assertServerEntryTable(ResultActions resultActions) throws Exception {
+        int expectedRowCount = 4;
+        String serverTable = "//table[@id='serverEntries']";
+
+        String friendlyNameCol = "td[1]";
+        String clusterCol = "td[2]";
+        String dnsStatusCol = "td[3]";
+        String actionCol = "td[4]";
+
+        resultActions
                 //server1
-                .andExpect(xpath("//tr").nodeCount(expectedRowCountWithHeader))
-                .andExpect(xpath("//tr[1]/td[1]").string("server" + laServer1.getId()))
-                .andExpect(xpath("//tr[1]/td[2]").string(USA))
-                .andExpect(xpath("//tr[1]/td[3]").string(USA + DOT_DOMAIN_COM))
-                .andExpect(xpath("//tr[1]/td[4]").string(Action.REMOVE.getDescription()))
+                .andExpect(xpath(serverTable + "//tr").nodeCount(expectedRowCount))
+                .andExpect(xpath(serverTable + "//tr[1]/" + friendlyNameCol).string("server" + laServer1.getId()))
+                .andExpect(xpath(serverTable + "//tr[1]/" + clusterCol).string(USA))
+                .andExpect(xpath(serverTable + "//tr[1]/" + dnsStatusCol).string(USA + DOT_DOMAIN_COM))
+                .andExpect(xpath(serverTable + "//tr[1]/" + actionCol).string(Action.REMOVE.getDescription()))
                 //server2
-                .andExpect(xpath("//tr[2]/td[1]").string("server" + laServer2.getId()))
-                .andExpect(xpath("//tr[2]/td[2]").string(USA))
-                .andExpect(xpath("//tr[2]/td[3]").string(USA + DOT_DOMAIN_COM))
-                .andExpect(xpath("//tr[2]/td[4]").string(Action.REMOVE.getDescription()))
+                .andExpect(xpath(serverTable + "//tr[2]/" + friendlyNameCol).string("server" + laServer2.getId()))
+                .andExpect(xpath(serverTable + "//tr[2]/" + clusterCol).string(USA))
+                .andExpect(xpath(serverTable + "//tr[2]/" + dnsStatusCol).string(USA + DOT_DOMAIN_COM))
+                .andExpect(xpath(serverTable + "//tr[2]/" + actionCol).string(Action.REMOVE.getDescription()))
                 //server20
-                .andExpect(xpath("//tr[3]/td[1]").string("server" + genevaServer.getId()))
-                .andExpect(xpath("//tr[3]/td[2]").string(SWITZERLAND))
-                .andExpect(xpath("//tr[3]/td[3]").string("NONE"))
-                .andExpect(xpath("//tr[3]/td[4]").string(Action.ADD.getDescription()));
+                .andExpect(xpath( serverTable + "//tr[3]/" + friendlyNameCol).string("server" + genevaServer.getId()))
+                .andExpect(xpath(serverTable + "//tr[3]/" + clusterCol).string(SWITZERLAND))
+                .andExpect(xpath(serverTable + "//tr[3]/" + dnsStatusCol).string("NONE"))
+                .andExpect(xpath(serverTable + "//tr[3]/" + actionCol).string(Action.ADD.getDescription()));
+    }
+
+    private static void assertDnsEntryTable(ResultActions resultActions) throws Exception {
+        int expectedRowCount = 4;
+        String dnsTable = "//table[@id='dnsEntries']";
+
+        String domainStringCol = "td[1]";
+        String ipCol = "td[2]";
+        String serverFriendlyNameCol = "td[3]";
+        String clusterNameCol = "td[4]";
+
+        resultActions
+                //server1
+                .andExpect(xpath(dnsTable + "//tr").nodeCount(expectedRowCount))
+                .andExpect(xpath(dnsTable + "//tr[1]/" + domainStringCol).string(LA + DOT_DOMAIN_COM))
+                .andExpect(xpath(dnsTable + "//tr[1]/" + ipCol).string(laServer1.getIpString()))
+                .andExpect(xpath(dnsTable + "//tr[1]/" + serverFriendlyNameCol).string(laServer1.getFriendlyName()))
+                .andExpect(xpath(dnsTable + "//tr[1]/" + clusterNameCol).string(laServer1.getCluster().getName()))
+                //server2
+                .andExpect(xpath(dnsTable + "//tr[2]/" + domainStringCol).string(LA + DOT_DOMAIN_COM))
+                .andExpect(xpath(dnsTable + "//tr[2]/" + ipCol).string(laServer2.getIpString()))
+                .andExpect(xpath(dnsTable + "//tr[2]/" + serverFriendlyNameCol).string(laServer2.getFriendlyName()))
+                .andExpect(xpath(dnsTable + "//tr[2]/" + clusterNameCol).string(laServer2.getCluster().getName()))
+                //server20
+                .andExpect(xpath( dnsTable + "//tr[3]/" + domainStringCol).string("xyz" + DOT_DOMAIN_COM))
+                .andExpect(xpath(dnsTable + "//tr[3]/" + ipCol).string("5.5.5.5"))
+                .andExpect(xpath(dnsTable + "//tr[3]/" + serverFriendlyNameCol).string("not found"))
+                .andExpect(xpath(dnsTable + "//tr[3]/" + clusterNameCol).string("N/A"));
     }
 
 }
