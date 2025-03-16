80081b448ebe3889a08ec262976fdb4988192bdb
alexdeturris <alexdeturris@hotmail.com>
2025-03-16 11:38:47 +0800

    fix redirection

---
+++ src/main/java/org/example/dnsservice/controller/Endpoints.java
@@ -0,0 +1,6 @@
+package org.example.dnsservice.controller;
+
+public class Endpoints {
+  public static final String BASE_URL = "/dns-service";
+  public static final String HOME = "/home";
+}
--- src/main/java/org/example/dnsservice/controller/HomeController.java
+++ src/main/java/org/example/dnsservice/controller/HomeController.java
@@ -1,5 +1,8 @@
 package org.example.dnsservice.controller;
 
+import static org.example.dnsservice.controller.Endpoints.BASE_URL;
+import static org.example.dnsservice.controller.Endpoints.HOME;
+
 import org.example.dnsservice.service.EntryStoreService;
 import org.example.dnsservice.service.UIService;
 import org.slf4j.Logger;
@@ -16,13 +19,12 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping(
-    value = {"/dns-service"},
+    value = {BASE_URL},
     produces = {"application/html"})
 public class HomeController {
 
   private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
-  private final String HOME = "/home";
   private final UIService uiService;
   private final EntryStoreService entryStoreService;
 
@@ -36,14 +38,14 @@ public class HomeController {
   public RedirectView add(@PathVariable Integer serverId) {
     entryStoreService.addToRotation(serverId);
     log.info("Successfully added server: {}. Redirecting home", serverId);
-    return new RedirectView(HOME);
+    return new RedirectView(BASE_URL + HOME);
   }
 
   @PostMapping("/remove/{serverId}")
   public RedirectView remove(@PathVariable Integer serverId) {
     entryStoreService.removeFromRotation(serverId);
     log.info("Successfully removed server: {}. Redirecting home", serverId);
-    return new RedirectView(HOME);
+    return new RedirectView(BASE_URL + HOME);
   }
 
   @GetMapping(HOME)
